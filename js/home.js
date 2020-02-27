5400273ce713c273e2632c6c2f8b4e34cc673152
Lichaha <kayinambao@gmail.com>
2020-02-27 21:36:09 +0200

    First Version of LichahaCodes

---
+++ js/home.js
@@ -0,0 +1,20 @@
+$('.carousel').carousel({
+	interval: 2000
+})
+
+function myForm(){
+alert("Coming soon!!");
+}
+
+function show(){
+	document.getElementById("zero").style.display = "inline-block";
+	document.getElementById("aa").style.display = "inline-block";
+}
+function hide(){
+	document.getElementById("zero").style.display = "none";
+	document.getElementById("aa").style.display = "none";
+}
+
+function pop() {
+	window.open('#', 'pop', "height=590, width=450, toolbar=no, location=no, status=no, menubar=no, scrollbars=auto, resizable=yes, copyhistory=no");
+}

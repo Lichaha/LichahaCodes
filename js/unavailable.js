f0cd80d587b137327946521d16b207af6779a6fb
Lichaha <54082232+Lichaha@users.noreply.github.com>
2020-05-29 22:30:53 +0200

    Update unavailable.js

--- js/unavailable.js
+++ js/unavailable.js
@@ -1,4 +1,4 @@
-var countDownDate = new Date("Mar 25, 2020 23:59:59").getTime();
+var countDownDate = new Date("Dec 25, 2020 23:59:59").getTime();
 
 var x = setInterval(function(){
 	var now = new Date().getTime();

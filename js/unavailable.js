6872e4fcf7f206dedbcecedbb6a61eefb44810bd
LichahaCodes <johnphiriqwerty@outlook.com>
2024-09-12 06:06:48 +0200

    Extended the unavailable date

--- js/unavailable.js
+++ js/unavailable.js
@@ -1,13 +1,13 @@
-var countDownDate = new Date("Dec 25, 2020 23:59:59").getTime();
+var countDownDate = new Date("Dec 25, 2024 23:59:59").getTime();
 
-var x = setInterval(function(){
+var x = setInterval(function () {
 	var now = new Date().getTime();
 	var distance = countDownDate - now;
 
-	var days = Math.floor(distance / (1000*60*60*24));
-	var hours = Math.floor((distance % (1000*60*60*24)) / (1000*60*60));
-	var minutes = Math.floor((distance % (1000*60*60)) / (1000*60));
-	var seconds = Math.floor((distance % (100*60)) / 1000);
+	var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+	var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+	var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+	var seconds = Math.floor((distance % (100 * 60)) / 1000);
 
 	document.getElementById("demo").innerHTML = days + "d  " + hours + "h  " + minutes + "m  " + seconds + "s ";
 
